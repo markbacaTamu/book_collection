d077b49289999047c2d17cb18fa4efcbc72e5375
markbacaTamu <markbaca42@tamu.edu>
2023-09-11 13:34:30 -0500

    removed extra migration file

--- db/migrate/20230905210308_add_columns_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToBooks < ActiveRecord::Migration[7.0]
-  def change
-    add_column :books, :author, :string
-    add_column :books, :price, :decimal
-    add_column :books, :published_date, :datetime
-  end
-end
